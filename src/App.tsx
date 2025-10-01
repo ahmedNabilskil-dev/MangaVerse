8088def87bc8e4c347670028afaf527ac9fa89b5
AhmedNabil1995 <ahmed.nabil55586@gmail.com>
2025-10-01 20:25:16 +0300

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,13 @@
 // In App.tsx
-import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 
 // In components using useSearchParams
 import LandingPage from "./pages/LandingPage";
 
 function App() {
   return (
-    <Router>
-      <div className="min-h-screen bg-white">
-        <Routes>
-          <Route path="/" element={<LandingPage />} />
-        </Routes>
-      </div>
-    </Router>
+    <div className="min-h-screen bg-white">
+      <LandingPage />
+    </div>
   );
 }
 
