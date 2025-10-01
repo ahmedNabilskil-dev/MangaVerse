fc328664936ddb1a342f7b4147d1cba0d0d1cdd8
AhmedNabil1995 <ahmed.nabil55586@gmail.com>
2025-10-01 20:34:08 +0300

    update

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,8 @@ import { defineConfig } from "vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 3000,
-  },
+  base: "/MangaVerse/",
+
   css: {
     postcss: "./postcss.config.js",
   },
