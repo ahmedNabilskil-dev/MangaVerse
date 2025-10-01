dccdfc115714528c134253804b34baef28a13304
AhmedNabil1995 <ahmed.nabil55586@gmail.com>
2025-10-01 20:26:52 +0300

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
 // In App.tsx
 
 // In components using useSearchParams
-import LandingPage from "./pages/LandingPage";
 
 function App() {
   return (
     <div className="min-h-screen bg-white">
-      <LandingPage />
+      aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
     </div>
   );
 }
